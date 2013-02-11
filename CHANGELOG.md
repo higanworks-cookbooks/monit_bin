072ab3b3af5fbf9a0ba1a28467f02997988bfb2c
sawanoboly <sawanoboriyu@higanworks.com>
2013-02-11 22:04:13 +0900

    defaut swap usage for smartos

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## v0.3.3
+
+- [Tune] Swap usage alert smartos.
+
 ## v0.3.2
 
 - [Bugfix] binpath to use attribute.
--- attributes/system.rb
+++ attributes/system.rb
@@ -7,3 +7,12 @@ default['monit']['check_system']['policies'] = [
   "if cpu usage (wait) > 50% for 3 cycles then alert"
 ]
 
+
+if node['platform'] == "smartos" then
+  default['monit']['check_system']['policies'] = [
+    "if loadavg(1min) > 15 for 5 cycles then alert",
+    "if loadavg(5min) > 10 for 5 cycles then alert",
+    "if swap usage > 85% for 3 cycles then alert",
+    "if cpu usage (wait) > 50% for 3 cycles then alert"
+  ]
+end
