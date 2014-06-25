0c1654b4171554e7209e269bcdd6dd42ce14434e
Aaron Rice <aaron@sneakybox.com>
2014-06-25 19:52:39 +0100

    Use debian provider service when on a deb platform

--- recipes/include.rb
+++ recipes/include.rb
@@ -3,7 +3,7 @@
 # Recipe:: default
 #
 # Copyright (C) 2012 HiganWorks LLC
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
 # "Software"), to deal in the Software without restriction, including
@@ -11,10 +11,10 @@
 # distribute, sublicense, and/or sell copies of the Software, and to
 # permit persons to whom the Software is furnished to do so, subject to
 # the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -34,5 +34,9 @@ service "monit" do
     reload_command "/opt/local/sbin/monit reload"
     start_command "/usr/sbin/init q"
     status_command "/usr/bin/pgrep /opt/local/sbin/monit"
+  when "debian"
+    provider Chef::Provider::Service::Init::Debian
+    supports :restart => true, :reload => true, :status => true
+    action :enable
   end
 end
